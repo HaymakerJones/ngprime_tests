ba666ea410ce4f5e9cfb8e0048d60ff6fbfe9c99
HaymakerJones <js388111@gmail.com>
2018-03-31 19:28:55 -0400

    added UUID middleware

--- src/app/datatable-demo/person.model.ts
+++ src/app/datatable-demo/person.model.ts
@@ -1,9 +1,13 @@
+import { UUID } from 'angular2-uuid';
+
 export class Person {
+    id: string;
     first: string;
     last: string;
     age: number;
 
     constructor(obj?: any) {
+        this.id = obj && obj.id || UUID.UUID();
         this.first = obj && obj.first || null;
         this.last = obj && obj.last || null;
         this.age = obj && obj.age || null;
